8899c1ebce913331475c62fdec60995138349ced
WarrenMfg <warrenmfg@icloud.com>
2020-05-08 14:14:30 -0400

    fixed formatting for server/index.js

--- build.js
+++ build.js
@@ -84,37 +84,37 @@ export default App;
 
 function server(port) {
   return `import express from 'express';
-  import morgan from 'morgan';
-  import { connect } from '../database/index';
-  import docRouter from '../doc/docRouter';
-  import { createGzip } from 'zlib';
-  import { createReadStream } from 'fs';
-  import { resolve } from 'path';
+import morgan from 'morgan';
+import { connect } from '../database/index';
+import docRouter from '../doc/docRouter';
+import { createGzip } from 'zlib';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
 
 
-  const PORT = process.env.PORT || ${Number(port) || 50000};
-  const app = express();
+const PORT = process.env.PORT || ${Number(port) || 50000};
+const app = express();
 
-  app.use(morgan('dev'));
-  app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.json());
 
-  app.use('/api', docRouter);
+app.use('/api', docRouter);
 
-  /*
-  uncomment before app deploys
-  app.get('/bundle.js', (req, res) => {
-    const gzip = createGzip();
-    const bundle = createReadStream(resolve(__dirname, '../../client/public/bundle.js'));
-    res.set({ 'Content-Encoding': 'gzip', 'Cache-Control': 'max-age=86400' });
-    bundle.pipe(gzip).pipe(res);
-  });
-  */
+/*
+uncomment before app deploys
+app.get('/bundle.js', (req, res) => {
+  const gzip = createGzip();
+  const bundle = createReadStream(resolve(__dirname, '../../client/public/bundle.js'));
+  res.set({ 'Content-Encoding': 'gzip', 'Cache-Control': 'max-age=86400' });
+  bundle.pipe(gzip).pipe(res);
+});
+*/
 
-  app.use('/', express.static('client/public'));
+app.use('/', express.static('client/public'));
 
-  connect();
-  app.listen(PORT, () => console.log('Listening on port', PORT));
-  `;
+connect();
+app.listen(PORT, () => console.log('Listening on port', PORT));
+`;
 }
 
 const crud =
